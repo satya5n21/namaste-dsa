3955201d4c1826af5472b4466d125ce1b6ce0632
satya5n21 <satyajitnayak348@gmail.com>
2026-09-08 05:07:54 +0530

    array-basic started

---
+++ array-basic/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let x = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > nums[x]) {
+            nums[x + 1] = nums[i];
+            x++;
+        }
+    }
+    return x + 1;
+};
+
+// [0,0,1,1,1,2,2,3,3,4]
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 5]));
+console.log(removeDuplicates([0]));
---
+++ array-basic/remove-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    let x = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[x] = nums[i];
+            x++;
+        }
+    }
+    return x;
+};
+
+
+console.log(removeElement([3, 2, 2, 3], 3));
+
+// [studied till 12:52] however problem is submitted and succeeded all testcase
