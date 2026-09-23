382867039a5c5d2b0cd7499ae19d2727a9f2262a
satya5n21 <satyajitnayak348@gmail.com>
2026-09-23 08:06:22 +0530

    linkedList implementation started

---
+++ linkedList-basic/design-singly-linked-list.js
@@ -0,0 +1,73 @@
+function Node(val) {
+    this.val = val;
+    this.next = null;
+}
+
+var MyLinkedList = function () {
+
+};
+
+/** 
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function (index) {
+
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtHead = function (val) {
+    let newNode = new Node(val);
+    newNode.next = this.addAtHead;
+    this.head = newNode;
+    this.size++;
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtTail = function (val) {
+    let newNode = new Node(val);
+    if (this.head == null) {
+        this.head = newNode;
+    } else {
+        let curr = this.head;
+        while (curr.next !== null) {
+            curr = curr.next;
+        }
+        curr.next = newNode;
+    }
+
+    this.size++;
+};
+
+/** 
+ * @param {number} index 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtIndex = function (index, val) {
+
+};
+
+/** 
+ * @param {number} index
+ * @return {void}
+ */
+MyLinkedList.prototype.deleteAtIndex = function (index) {
+
+};
+
+/** 
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
